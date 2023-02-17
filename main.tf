786c865acfad092df5025fc9193f25e865b4d166
alvaro_zenki <alvaro@zenki.fi>
2023-02-17 13:27:52 -0600

    Se comentan credenciales provider

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 # -------------------------
 provider "aws" {
   region                   = "us-east-1"
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                  = "local"
+  #shared_credentials_files = ["~/.aws/credentials"]
+  #profile                  = "local"
 }
 
 variable "usuarios" {
